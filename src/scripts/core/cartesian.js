48302e2c87077a5e8b2d0154005466885ea8cb8c
Jaime del Palacio <jaimedelpalacio@forio.com>
2013-09-19 16:54:13 -0700

    Implemented base cartesian frame with xScaling

--- src/scripts/core/cartesian.js
+++ src/scripts/core/cartesian.js
@@ -1,5 +1,66 @@
 (function (ns, d3, _, $, undefined) {
 
+    var defaults = {
+        xAxis: {
+            rangePadding: 0
+        }
+    };
 
-    window[ns].expose('cartesian', frame);
+    var cartesian = {
+
+        init: function () {
+            _.extend(this.options, defaults);
+            return this;
+        },
+
+        // to be overriden when coputing scales
+        // xScale: function (x) { return x; },
+        yScale: function (y) { return y; },
+        computeXScale: function () {
+            if (!this.xDomain) throw new Error('You are trying to render without setting data.');
+
+            this.xScale = d3.scale.ordinal()
+                .domain(this.xDomain)
+                .rangeRoundBands([0, this.options.chart.plotWidth]);
+        },
+
+        computeYScale: function () {
+
+        },
+
+        computeScales: function () {
+            this.computeXScale();
+            this.computeYScale();
+        },
+
+        render: function () {
+            this.computeScales();
+            this.baseRender();
+
+            return this;
+        },
+
+        datum: function (d, index) {
+            return {
+                y: _.isObject(d) ? d.y : d,
+                x: _.isObject(d) ? d.x : index
+            };
+        },
+
+        data: function (series) {
+
+            if (series instanceof Array) {
+                var datums = _.map(series, this.datum);
+                this.dataSrc = datums;
+
+                // this has to be the same for all series?
+                this.xDomain = _.pluck(datums, 'x');
+            }
+
+            return this;
+        }
+    };
+
+    window[ns].prototype.expose('cartesian', cartesian);
+
 })('Narwhal', window.d3, window._, window.jQuery);
--- tests/specs/cartesian-spec.js
+++ tests/specs/cartesian-spec.js
@@ -1,4 +1,4 @@
-describe('Cartesian', function () {
+describe('Cartesian frame', function () {
     var $el, el;
     var narwhal;
 
@@ -9,18 +9,46 @@ describe('Cartesian', function () {
 
     function createNarwhal(options) {
         options = _.extend({ el: el }, options);
-        narwhal = new Narwhal(options);
+        narwhal = new Narwhal(options).cartesian();
         return narwhal;
     }
 
+    it('should expose "cartesian" constructor function', function () {
+        expect(Narwhal.prototype.cartesian).toBeDefined();
+    });
 
     describe('constructor', function () {
-
-
     });
 
     describe('render', function () {
+        // this should be in the render, because is data dependent
+        it('should provide scaling function (xScale & yScale) for visualizations to use', function () {
+            var target = createNarwhal()
+                .data([1,2,3])
+                .render();
 
+            expect(target.xScale).toBeDefined();
+            expect(target.yScale).toBeDefined();
+        });
+    });
+
+    describe('default xScale', function () {
+        beforeEach(function () {
+            narwhal = createNarwhal();
+        });
+
+        it('should throw if no data is set', function () {
+            expect(narwhal.render).toThrow();
+        });
+
+        it('should be an ordinal scaling', function () {
+            narwhal.data([0,10,20,30]).render();
+
+            expect(narwhal.xScale(0)).toEqual(0);
+            expect(narwhal.xScale(1)).toEqual(100);
+            expect(narwhal.xScale(2)).toEqual(200);
+            expect(narwhal.xScale(3)).toEqual(300);
+        });
 
     });
 
