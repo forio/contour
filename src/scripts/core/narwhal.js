3dce56920e668d297c10185d8461ef4623701474
Jaime del Palacio <jaimedelpalacio@forio.com>
2013-09-19 11:20:32 -0700

    Basic functionality & tests for svg positioning and margins

--- src/scripts/core/narwhal.js
+++ src/scripts/core/narwhal.js
@@ -2,26 +2,23 @@
 
     var defaults = {
         chart: {
-            width: 400,
-            height: 280,
-            /* margin between the container and the chart */
+            width: '100%',      // by default take the size of the parent container
+            height: undefined,  // if defined, height takes precedence over aspect
+            aspect: 1.61,       // height = width / 1.61
+            /* margin between the container and the chart (ie labels or axis title) */
             margin: {
                 top: 0,
                 right: 0,
                 bottom: 0,
                 left: 0
             },
-            /* padding between the chart area and the plot area */
+            /* padding between the chart area and the inner plot area */
             padding: {
                 top: 0,
                 right: 0,
                 bottom: 0,
                 left: 0
             },
-        },
-        title: {
-            height: 0,
-            text: ''
         }
     };
 
@@ -33,28 +30,52 @@
     Narwhal.prototype = _.extend(Narwhal.prototype, {
 
         init: function (options) {
-            options = $.extend(true, {}, defaults, options);
-
-            this.options = _.extend(options, {
-                plotWidth: options.chart.width - options.chart.margin.left - options.chart.margin.right - options.chart.padding.left - options.chart.padding.right,
-                plotHeight: options.chart.height - options.chart.margin.top - options.chart.margin.bottom - options.chart.padding.top - options.chart.padding.bottom
-            });
+            this.options = $.extend(true, {}, defaults, options);
 
             return this;
         },
 
+        calcMetrics: function () {
+            var options = this.options;
+
+            this.options = $.extend(true, options, {
+                plotWidth: options.chart.width - options.chart.margin.left - options.chart.margin.right - options.chart.padding.left - options.chart.padding.right,
+                plotHeight: options.chart.height - options.chart.margin.top - options.chart.margin.bottom - options.chart.padding.top - options.chart.padding.bottom
+            });
+        },
+
         plotArea: function () {
+            this.calcMetrics();
+
+            var chartOpt = this.options.chart;
+
             this.container = d3.select(this.options.el);
             this.svg = this.container
                 .append('svg')
-                    .attr('width', options.plotWidth)
-                    .attr('height', options.plotHeight);
+                    .attr('width', chartOpt.width)
+                    .attr('height', chartOpt.height)
+                .append('g')
+                    .attr('transform', 'translate(' + chartOpt.margin.left + ',' + chartOpt.margin.top + ')');
 
             return this;
         },
 
         render: function () {
             this.plotArea();
+
+            return this;
+        },
+
+        expose: function (name, functionality) {
+            var ctorObj = {};
+            var ctor = _.bind(function () {
+                // extend the --instance-- we don't want all charts to be overriten...
+                _.extend(this, functionality);
+            }, this);
+
+            ctorObj[name] = ctor;
+
+            _.extend(this, ctorObj);
 
             return this;
         }
--- tests/specs/core-spec.js
+++ tests/specs/core-spec.js
@@ -5,33 +5,76 @@ describe('Narwhal', function () {
     beforeEach(function () {
         $el = $('<div>');
         el = $el.get(0);
+    });
 
-        narwhal = new Narwhal({
-            el: el
+    function createNarwhal(options) {
+        options = _.extend({ el: el }, options);
+        narwhal = new Narwhal(options);
+        return narwhal;
+    }
+
+    describe('expose', function () {
+        it('should not expose passed in object, until the constructor is called', function () {
+            var target = createNarwhal();
+            expect(target.myTest).not.toBeDefined();
+
+            // extend the instance, exposing a new constructor myTest
+            target.expose('myTest', { someFunction: $.noop });
+
+            // now we should have it, call the constructor
+            expect(target.myTest).toBeDefined();
+
+            target.myTest();
+
+            // now we should have the actual functionality exposed by myTest
+            expect(target.someFunction).toBeDefined();
+        });
+
+        it('should make the new object available in the instance', function () {
+            var target = createNarwhal();
+            target.expose('myTest', { someFunction: $.noop });
+            expect(target.myTest).toBeDefined();
+        });
+
+        it('should NOT affect other instance of Narwhal', function () {
+            var target = createNarwhal();
+            var other = createNarwhal();
+            target.expose('myTest', { someFunction: $.noop });
+            expect(other.myTest).not.toBeDefined();
         });
     });
 
-    describe('Render', function () {
+    describe('render', function () {
 
         beforeEach(function () {
-            narwhal.render();
         });
 
         it('should create an svg element inside the container', function () {
+            createNarwhal().render();
             expect($el.find('svg').length).toEqual(1);
         });
 
         it('should take the dimensions from the options', function () {
-            narwhal = new Narwhal({
-                el: el,
-                width: 100,
-                height: 100
-            });
+            createNarwhal({ chart: { width: 100, height: 100 } }).render();
 
-            var width = $el.find('svg').attr('width');
-            var height = $el.find('svg').attr('height');
-            expect(width).toEqual('100px');
-            expect(height).toEqual('100px');
+            var width = +$el.find('svg').attr('width');
+            var height = +$el.find('svg').attr('height');
+            expect(width).toEqual(100);
+            expect(height).toEqual(100);
+        });
+
+        it('should position chart area using the provided margins', function () {
+            createNarwhal({
+                chart: {
+                    width: 100,
+                    height: 100,
+                    margin: { left: 20, right: 20, top: 10, bottom: 10 }
+                }
+            }).render();
+
+            var svg = $el.find('svg');
+            var transform = svg.find('g').attr('transform');
+            expect(transform).toEqual('translate(20,10)');
         });
 
     });
