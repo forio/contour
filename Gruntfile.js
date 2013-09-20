945f70321cada0c0053a63ad33f250724e8a1186
Jaime del Palacio <jaimedelpalacio@forio.com>
2013-09-19 17:18:15 -0700

    added generic grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,187 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less']
+            },
+            js: {
+                files: ['src/scripts/**/*.js'],
+                tasks: ['concat', 'include']
+            }
+        },
+        less: {
+            dev: {
+                files: {
+                    'dist/css/narwhal.css': 'src/styles/narwhal.less'
+                }
+            },
+            production: {
+                options: {
+                    yuicompress: false // Off until calc() bug is addressed.  https://github.com/yui/yuicompressor/issues/59
+                },
+                files: {
+                    'dist/css/narwhal.min.css': 'src/styles/narwhal.less'
+                }
+            }
+        },
+        scripts: {
+            libs: [
+            ],
+            core: [
+            ]
+        },
+        include: {
+            core: {
+                src: ['<%= scripts.lib %>', '<%= scripts.core %>'],
+                dest: 'web/include/login/scripts.html'
+            }
+        },
+        concat: {
+            faculty: {
+                src: ['<%= scripts.lib %>', '<%= scripts.core %>'],
+                dest: 'dist/narwal.js'
+            }
+        },
+        uglify: {
+            options: {
+                sourceMap: function (fileName) {
+                    return fileName.replace(/\.js$/, '.map');
+                },
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+                        '<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            production: {
+                files: {
+                    'dist/narwhal.min.js': ['dist/narwhal.js']
+                }
+            }
+        }
+    });
+
+    grunt.registerMultiTask('templates', 'Compiles underscore templates', function () {
+        var _ = require('lodash');
+        var options = this.options({
+            separator: grunt.util.linefeed,
+            templateSettings: {},
+            templateNamespace: 'tmpl'
+        });
+
+        function getNamespace(ns) {
+            var output = [];
+            var curPath = 'App';
+            if (ns !== 'App') {
+                var nsParts = ns.split('.');
+                nsParts.forEach(function(curPart, index) {
+                    if (curPart !== 'App') {
+                        curPath += '[' + JSON.stringify(curPart) + ']';
+                        output.push(curPath + ' = ' + curPath + ' || {};');
+                    }
+                });
+            }
+
+            return {
+                namespace: curPath,
+                declaration: output.join('\n')
+            };
+        }
+
+        this.files.forEach(function (f) {
+            var namespaces = [];
+            var output = f.src.filter(function (path) {
+                if (!grunt.file.exists(path)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function (path) {
+                var src = grunt.file.read(path);
+                var parts = path.match(/.+\/(?:template|tmpl)(?:s)?\/(.+).html/i)[1].split('/');
+                var ns = _.initial(parts).join('.').replace(/(-.)/g, function () {
+                    return arguments[0].substring(1).toUpperCase();
+                });
+                var name = _.last(parts).replace(/(-.)/g, function () {
+                    return arguments[0].substring(1).toUpperCase();
+                });
+                var nsInfo = getNamespace([options.templateNamespace, ns].join('.'));
+                var compiled;
+
+                try {
+                    compiled = _.template(src, false, options.templateSettings).source;
+                } catch (e) {
+                    grunt.log.error(e);
+                    grunt.fail.warn('_.template failed to compile in file "' + filepath + '"');
+                }
+
+                if (options.prettify) {
+                    compiled = compiled.replace(new RegExp('\n', 'g'), '');
+                }
+
+                namespaces = [].concat(namespaces, nsInfo.declaration.split('\n'));
+
+                return nsInfo.namespace + '["' + name + '"] = ' + compiled + ';';
+            });
+
+            if (output.length < 1) {
+                grunt.log.warn('Destination not written because compiled files were empty.');
+            } else {
+                if (options.templateNamespace !== false) {
+                    output.unshift(_.uniq(namespaces).join('\n'));
+                }
+
+                output.unshift('(function (App, undefined) {');
+                output.push('})( this["' + options.namespace + '"]);');
+
+                grunt.file.write(f.dest, output.join(grunt.util.normalizelf(options.separator)));
+                grunt.log.writeln('File "' + f.dest + '" created.');
+            }
+        });
+
+    });
+
+    grunt.registerMultiTask('include', 'Inserts all required script tags into the html at a special marked insertion point', function () {
+        var _ = require('lodash');
+        var options = this.options({
+            separator: grunt.util.linefeed,
+            attributes: '',
+            webRoot: 'web/'
+        });
+        var reRoot = new RegExp('^' + options.webRoot);
+
+        this.files.forEach(function (f) {
+            var output = f.src.filter(function (path) {
+                if (!grunt.file.exists(path)) {
+                    grunt.log.warn('Source file "' + filepath + '" not found.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }).map(function (path) {
+                return _.compact(['<script', options.attributes, 'src="' + path.replace(reRoot, '') + '"></script>']).join(' ');
+            });
+
+            if (output.length < 1) {
+                grunt.log.warn('Destination not written because compiled files were empty.');
+            } else {
+                grunt.file.write(f.dest, output.join(grunt.util.normalizelf(options.separator)));
+                grunt.log.writeln('File "' + f.dest + '" created.');
+            }
+        });
+
+    });
+
+
+    // Default task.
+    grunt.registerTask('default', ['less:dev', 'watch']);
+    grunt.registerTask('production', ['concat', 'uglify', 'less:production']);
+};
