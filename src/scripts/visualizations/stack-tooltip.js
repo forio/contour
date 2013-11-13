82f40502934c6de0e1bb1ba9ca3d005d39887050
Jaime del Palacio <jaimedelpalacio@forio.com>
2013-11-13 11:41:42 -0800

    Fixed stacked tooltip to not show null/undefined values in series

--- src/scripts/visualizations/stack-tooltip.js
+++ src/scripts/visualizations/stack-tooltip.js
@@ -6,8 +6,11 @@ Narwhal.export('stackTooltip', function (data, layer, options) {
     tooltip.addClass('stack-tooltip');
 
     var onMouseOver = function (d) {
-        var filtered = _.filter(_.map(data, function (p, i) { return p.data[d.x] ? { z: p.name, y: p.data[d.x].y, class: 's-' + (i+1) } : null; }), function (x) { return x; });
-        var text = _.map(filtered, function (t) { return '<span class="' + t.class + '"">' + t.z + ': ' + valueFormatter(t.y) + '</span>'; }).join(' / ');
+        var isNull = function (p) {
+            return !(p && p.y != null);
+        };
+        var filtered = _.filter(_.map(data, function (p, i) { return !isNull(p.data[d.x]) ? { seriesName: p.name, value: p.data[d.x].y, cssClass: 's-' + (i+1) } : null; }), function (x) { return x; });
+        var text = _.map(filtered, function (t) { return '<span class="' + t.cssClass + '"">' + t.seriesName + ': ' + valueFormatter(t.value) + '</span>'; }).join(' / ');
         tooltip.html(text).show();
     };
 
