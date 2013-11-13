52be793fc9739fed8dbdb21980fbda26e4523a5b
Jaime del Palacio <jaimedelpalacio@forio.com>
2013-11-12 17:08:50 -0800

    Update stack tooltip to use the default formatter for values

--- src/scripts/visualizations/stack-tooltip.js
+++ src/scripts/visualizations/stack-tooltip.js
@@ -1,11 +1,13 @@
 Narwhal.export('stackTooltip', function (data, layer, options) {
 
+    var valueFormatter = this.yAxis().tickFormat();
     var tooltip = $(options.stackTooltip.el);
+
     tooltip.addClass('stack-tooltip');
 
     var onMouseOver = function (d) {
         var filtered = _.filter(_.map(data, function (p, i) { return p.data[d.x] ? { z: p.name, y: p.data[d.x].y, class: 's-' + (i+1) } : null; }), function (x) { return x; });
-        var text = _.map(filtered, function (t) { return '<span class="' + t.class + '"">' + t.z + ': ' + t.y + '</span>'; }).join(' / ');
+        var text = _.map(filtered, function (t) { return '<span class="' + t.class + '"">' + t.z + ': ' + valueFormatter(t.y) + '</span>'; }).join(' / ');
         tooltip.html(text).show();
     };
 
