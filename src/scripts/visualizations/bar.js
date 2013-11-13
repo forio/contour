06fea1079b961689b899ba5758744a5f0c4d5cda
Jaime del Palacio <jaimedelpalacio@forio.com>
2013-11-12 16:10:31 -0800

    clean up

--- src/scripts/visualizations/bar.js
+++ src/scripts/visualizations/bar.js
@@ -5,36 +5,23 @@
         var yScale = this.yScale;
         var rangeBand = this.rangeBand;
         data = data[0].data ? data : [{ name: 's1', data:data }];
-        var stack = d3.layout.stack()
-            .values(function (d) {
-                return d.data;
-            });
+        var classFn = function (d, i) { return 'series s-' + (i+1) + ' ' + d.name; };
+        var stack = d3.layout.stack().values(function (d) { return d.data; });
 
         var series = layer.selectAll('g.series')
                 .data(stack(data))
                 .enter().append('svg:g')
-                    .attr('class', function (d, i) {
-                        return 'series s-' + (i+1) + ' ' + d.name;
-                    });
+                    .attr('class', classFn);
 
         var bar = series.selectAll('.bar')
                 .data(function (d) { return d.data; });
 
         bar.enter().append('rect')
             .attr('class', 'bar tooltip-tracker')
-            .attr('y', function (d) {
-                return xScale(d.x);
-            })
-            .attr('height', rangeBand )
-
-            .attr('x', function (d) {
-                return yScale(d.y0 || 0);
-            })
-            .attr('width', function (d) {
-                return yScale(d.y);
-            });
-
-
+            .attr('y', function (d) { return xScale(d.x); })
+            .attr('height', rangeBand)
+            .attr('x', function (d) { return yScale(d.y0 || 0); })
+            .attr('width', function (d) { return yScale(d.y); });
     });
 
 }).call(this);
