80f30d398273cf8e6c390a455ee9e1ce30df96c5
Jaime del Palacio <jaimedelpalacio@forio.com>
2013-11-12 16:40:29 -0800

    Implemented basic tooltip for stacked charts (where we show all elements of the stack)

---
+++ src/scripts/visualizations/stack-tooltip.js
@@ -0,0 +1,19 @@
+Narwhal.export('stackTooltip', function (data, layer, options) {
+
+    var tooltip = $(options.stackTooltip.el);
+    tooltip.addClass('stack-tooltip');
+
+    var onMouseOver = function (d) {
+        var filtered = _.filter(_.map(data, function (p, i) { return p.data[d.x] ? { z: p.name, y: p.data[d.x].y, class: 's-' + (i+1) } : null; }), function (x) { return x; });
+        var text = _.map(filtered, function (t) { return '<span class="' + t.class + '"">' + t.z + ': ' + t.y + '</span>'; }).join(' / ');
+        tooltip.html(text).show();
+    };
+
+    var onMouseOut = function (d) {
+        tooltip.html('');
+    };
+
+    this.svg.selectAll('.tooltip-tracker')
+        .on('mouseover.tooltip', onMouseOver.bind(this))
+        .on('mouseout.tooltip',  onMouseOut.bind(this));
+});
