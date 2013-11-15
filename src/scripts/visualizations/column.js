045034ed75255e00ecbcc4a380f0323d89ef0572
Jaime del Palacio <jaimedelpalacio@forio.com>
2013-11-15 10:56:32 -0800

    Implemented column stacked & grouped

--- src/scripts/visualizations/column.js
+++ src/scripts/visualizations/column.js
@@ -1,40 +1,49 @@
 (function (ns, d3, _, $, undefined) {
 
     var defaults = {
+        column : {
+            stacked: false,
+            padding: 1
+        }
     };
 
-    function render(data, layer, options, id) {
+    function render(data, layer, options) {
+        var opt = options.column;
         var h = this.options.chart.plotHeight;
         var x = this.xScale;
         var y = this.yScale;
-        var colWidth = this.rangeBand;
-        var min = this.options.yAxis.min || this.yMin;
+        var rangeBand = this.rangeBand;
+        var classFn = function (d, i) { return 'series s-' + (i+1) + ' ' + d.name; };
+        var stack = d3.layout.stack().values(function (d) { return d.data; });
+        var enter = this.options.column.stacked ? stacked : grouped;
 
-        // for now just user the first series
-        var series = data[0];
-        var seriesName = series.name;
+        var series = layer.selectAll('g.series')
+                .data(stack(data))
+                .enter().append('g')
+                    .attr('class', classFn);
 
-        var col = layer.selectAll('.column')
-            .data(series.data);
-
+        var col = series.selectAll('.column')
+            .data(function(d) { return d.data; });
         col.enter().append('rect')
-                .attr('class', 'column v-'+ id + ' s-1 ' + seriesName)
-                .attr('x', function (d) { return x(d.x); })
-                .attr('y', function () { return y(min); })
-                .attr('height', 0)
-                .attr('width', function () { return colWidth; });
+            .attr('class', 'column tooltip-tracker');
 
-        if (this.options.chart.animations) {
-            var delay = 3;
-            col.transition().delay(function (d, i) { return i * delay; })
-                .duration(500)
-                .attr('height', function (d) { return h - y(d.y); })
-                .attr('y', function (d) { return y(d.y); });
+        enter.call(this, col);
 
-        } else {
-            col.attr('height', function (d) { return h - y(d.y); })
-                .attr('y', function (d) { return y(d.y); });
+        function stacked(col) {
+            col.attr('x', function (d) { return x(d.x); })
+                .attr('width', function () { return rangeBand; })
+                .attr('y', function (d) { return y(d.y) + y(d.y0) - h; })
+                .attr('height', function (d) { return h - y(d.y); });
+        }
 
+        function grouped(col) {
+            var width = rangeBand / data.length - opt.padding;
+            var offset = function (d, i) { return rangeBand / data.length * i; };
+
+            col.attr('x', function (d, i, j) { return x(d.x) + offset(d, j); })
+                .attr('width', width)
+                .attr('y', function (d) { return y(d.y); })
+                .attr('height', function (d) { return h - y(d.y); });
         }
     }
 
