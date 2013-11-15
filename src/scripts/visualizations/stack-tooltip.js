99530ebe43157351fd32669f668578b5d691a636
Jaime del Palacio <jaimedelpalacio@forio.com>
2013-11-15 11:24:12 -0800

    Fixed stacked tooltip to work with categorized series data

--- src/scripts/visualizations/stack-tooltip.js
+++ src/scripts/visualizations/stack-tooltip.js
@@ -5,16 +5,23 @@ Narwhal.export('stackTooltip', function (data, layer, options) {
 
     tooltip.addClass('stack-tooltip');
 
+    /*jshint eqnull:true*/
     var onMouseOver = function (d) {
         var isNull = function (p) {
             return !(p && p.y != null);
         };
-        var filtered = _.filter(_.map(data, function (p, i) { return !isNull(p.data[d.x]) ? { seriesName: p.name, value: p.data[d.x].y, cssClass: 's-' + (i+1) } : null; }), function (x) { return x; });
+        var mapFn = function (p, i) {
+            var index = _.isNumber(d.x) ? d.x : options.xAxis.categories.indexOf(d.x);
+            return !isNull(p.data[index]) ?
+                { seriesName: p.name, value: p.data[index].y, cssClass: 's-' + (i+1) } :
+                null;
+        };
+        var filtered = _.filter(_.map(data, mapFn), function (x) { return x; });
         var text = _.map(filtered, function (t) { return '<span class="' + t.cssClass + '"">' + t.seriesName + ': ' + valueFormatter(t.value) + '</span>'; }).join(' / ');
         tooltip.html(text).show();
     };
 
-    var onMouseOut = function (d) {
+    var onMouseOut = function (/* datum */) {
         tooltip.html('');
     };
 
