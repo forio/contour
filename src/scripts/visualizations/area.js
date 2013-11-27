e50c090b49f1dc82dc1d1154858063a3373258cc
Jaime del Palacio <jaimedelpalacio@forio.com>
2013-11-27 10:46:01 -0800

    Implemented stacked area chart

--- src/scripts/visualizations/area.js
+++ src/scripts/visualizations/area.js
@@ -7,27 +7,25 @@
     };
 
     function renderer(data, layer, options) {
-        var x = _.bind(function (d) { return this.xScale(d.x) + this.rangeBand / 2; }, this);
-        var y = _.bind(function (d) { return this.yScale(d.y); }, this);
+        var x = _.bind(function (val) { return this.xScale(val) + this.rangeBand / 2; }, this);
+        var y = _.bind(function (val) { return this.yScale(val); }, this);
+        var classFn = function (d, i) { return 'series s-' + (i+1) + ' ' + d.name; };
+        var stack = d3.layout.stack().values(function (d) { return d.data; });
+        var area = d3.svg.area()
+            .x(function(d) { return x(d.x); })
+            .y0(function (d) { return y(d.y0 || 0); })
+            .y1(function(d) { return y(d.y0 + d.y); });
 
-        _.each(data, renderSeries, this);
+        var series = layer.selectAll('g.series')
+                .data(stack(data))
+                .enter().append('g')
+                    .attr('class', classFn);
 
-        function renderSeries(series, index) {
-            var seriesName = function () { return series.name.replace(' ', '_') + ' s-' + (index+1); };
-            var area = d3.svg.area()
-                .x(function(d) { return x(d); })
-                .y0(this.options.chart.plotHeight)
-                .y1(function(d) { return y(d); });
-
-            var g = layer.append('g')
-                    .attr('class', seriesName());
-
-            g.append('path')
-                .datum(series.data)
-                .attr('class', 'area')
-                .attr('d', area);
-
-        }
+        series
+            .append('path')
+            .datum(function(d) { return d.data; })
+            .attr('class', 'area')
+            .attr('d', area);
 
     }
 
