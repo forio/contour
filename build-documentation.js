8de0a22019b4145d7eab40047a05cb7657c0fffa
Jaime del Palacio <jaimedelpalacio@forio.com>
2013-12-09 09:49:27 -0800

    Added a script to extract configuration file documentation

--- build-documentation.js
+++ build-documentation.js
@@ -1,14 +1,15 @@
 
 var glob = require('glob');
 var fs = require('fs');
-// var confox = require('./confox');
+var confdox = require('./config-dox');
 var markdox = require('markdox');
 var async = require('async');
 var path = require('path');
-var _ = require('underscore');
+var _ = require('lodash');
 
 var docFolder = __dirname + '/documentation';
-
+var configDocFolder = docFolder + '/config/';
+var propTemplatePath = docFolder + '/config/prop.template.md';
 
 var options = {
     src: [
@@ -18,41 +19,102 @@ var options = {
         'src/scripts/core/**/*.js',
         'src/scripts/visualizations/**/*.js',
         'src/scripts/**/*.js'
-    ]
+    ],
+
+    configTarget: 'config'
 };
 
+var allFiles = getSourceFileList(options.src);
 
-var allFiles = [];
-options.src.forEach(function (spec) {
-    glob(spec, { sync: true, nosort: true }, function (er, files) {
-        allFiles = _.union(allFiles, files);
+// generatePerFileDoc(allFiles);
+// generateAllFilesDoc(allFiles);
+generateConfigObjectDoc(allFiles, options.configTarget);
+
+
+
+
+function getSourceFileList(srcSpec) {
+    var allFiles = [];
+    srcSpec.forEach(function (spec) {
+        glob(spec, { sync: true, nosort: true }, function (er, files) {
+            allFiles = _.union(allFiles, files);
+        });
     });
-});
 
-// One file per Javascript file
-async.forEach(allFiles, function(file, next) {
-    var output = docFolder + '/' + path.basename(file) + '.md';
-    markdox.process(file, output, next);
-}, function(err) {
-    if (err) {
-        throw err;
+    return allFiles;
+}
+
+function generatePerFileDoc(allFiles) {
+    // One file per Javascript file
+    async.forEach(allFiles, function(file, next) {
+        var output = docFolder + '/' + path.basename(file) + '.md';
+        markdox.process(file, output, next);
+    }, function(err) {
+        if (err) {
+            throw err;
+        }
+
+        console.log('Documents generated with success');
+    });
+}
+
+function generateAllFilesDoc(allFiles) {
+    // One file for all Javascript files
+    var output = docFolder + '/all.md';
+    markdox.process(allFiles, {output:output}, function() {
+        console.log('File `all.md` generated with success');
+    });
+}
+
+
+function generateConfigObjectDoc(files, targetDir) {
+    var configObject = {};
+    files.forEach(function (file) {
+        var js = fs.readFileSync(file).toString();
+        var res = confdox.parseConfigObject(js);
+
+        // the order of the files is from generic to specific
+        // and we want to document through the global config file
+        // the most generic version of the config property
+        configObject = _.merge({}, res, configObject);
+    });
+
+    fs.readFile(propTemplatePath, 'utf8', function (err, template) {
+        var compiled = _.template(template);
+
+        bfs(configObject, 'config', function (k, isParent) {
+            var fileName = configDocFolder + (k.ctx ? k.ctx + '.' + k.key : k.key);
+            var md = compiled({
+                name: k.key,
+                type: typeof k.ref,
+                description: '',
+                defaultValue: k.ref === null ? 'null' : k.ref === undefined ? 'undefined' : k.ref.toString()
+            });
+
+            fs.writeFile(fileName, md, 'utf8');
+        });
+
+    });
+}
+
+function bfs(root, rootContext, visitor) {
+    var queue = [];
+
+    queue.push({key: rootContext, ref: root, ctx: '' });
+
+    while(queue.length) {
+        var cur = queue.shift();
+        var parent = typeof cur.ref === 'object';
+        visitor(cur, parent);
+        // console.log('testing ' + cur.key + ' -> ' + typeof cur.ref);
+        if (parent) {
+            for(var key in cur.ref) {
+                context = cur.ctx ? cur.ctx + '.' + cur.key : cur.key;
+                queue.push({key: key, ref: cur.ref[key], ctx: context });
+            }
+        }
     }
+}
 
-    console.log('Documents generated with success');
-});
-
-
-// One file for all Javascript files
-var output = docFolder + '/all.md';
-markdox.process(allFiles, {output:output}, function() {
-    console.log('File `all.md` generated with success');
-});
-
-
-// files.forEach(function (file) {
-//     var js = fs.readFileSync(file).toString();
-//     var res = confox.parseConfigObject(js);
-//     console.log(res);
-// });
 
 
---
+++ config-dox.js
@@ -0,0 +1,118 @@
+var _ = require('underscore');
+
+
+String.prototype.regexIndexOf = function(regex, start) {
+    var indexOf = this.substring(start || 0).search(regex);
+    return (indexOf >= 0) ? (indexOf + (start || 0)) : indexOf;
+};
+
+
+var isSafeForEval = function(text) {
+    // from json2 parser
+    // for details goto: https://github.com/douglascrockford/JSON-js/blob/master/json2.js
+    // return /^[\],:{}\s]*$/.test(text.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@')
+    //         .replace(/["'][^"\\\n\r]*["']|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
+    //         .replace(/(?:^|:|,)(?:\s*\[)+/g, ''));
+
+    return true;
+};
+
+/*jslint evil: true, regexp: true */
+var getConfigObject = function (js, options) {
+    options = options || {};
+    var searchPatterns = [
+        '\\/\\*\\*\\s?@config',
+        'defaults\\s?=\\s?\\{'
+    ].join('|');
+
+    var defaults = {
+        patterns: searchPatterns
+    };
+
+    options = _.extend(defaults, options);
+
+    // search for the 'config object' accounting to the given patterns
+    var regExp = new RegExp(options.patterns, 'g');
+    if(!(regExp.test(js))) return {};
+
+    var start = js.regexIndexOf(regExp);
+    if(js[start] === '/') {
+        start = js.regexIndexOf(/\*\//, start);
+    }
+
+    var firstBraket = js.regexIndexOf(/{/, start);
+    var buf = [], brakets = 0, cur = firstBraket;
+    var whithinComment = false, whithinMultilineComment = false;
+
+    do {
+        var token = js[cur++];
+        var next = js[cur];
+
+        switch(token) {
+        case '\n':
+            whithinComment = false;
+            break;
+        case '*':
+            if (next === '/') whithinMultilineComment = false;
+            break;
+        case '/':
+            if (next === '/') whithinComment = true;
+            if (next === '*') whithinMultilineComment = true;
+            break;
+        case '{':
+            // only count the braket if we our not in a comment block|line
+            brakets += !(whithinComment || whithinMultilineComment) | 0;
+            break;
+        case '}':
+            // only count the braket if we our not in a comment block|line
+            brakets -= !(whithinComment || whithinMultilineComment) | 0;
+            break;
+        }
+
+        buf.push(token);
+    } while(brakets);
+
+    var text = buf.join('');
+    if(!isSafeForEval(text)) throw new SyntaxError('Can\'t parse config object');
+
+    // aaaahhhh!!! people are going to die with this!!!
+    return eval('(' + text + ')');
+};
+
+var res = {
+    "tags":[
+        {
+            "type": "config-object",
+            "description": "configuration object for ABC visualization"
+        }
+    ],
+    "desciption": {
+        "full": "full description",
+        "summary": "first line of description",
+        "body": "body of description"
+    },
+    "ctx": {
+        "type": "property",
+        "receiver": "window",
+        "name": "config"
+    }
+};
+
+
+exports.parseConfigObject = function(js, options) {
+    options = options || {};
+
+    var config = getConfigObject(js);
+    var doc = {
+        "tags": [{ "type": "config-object", "description": "" }],
+        "ctx": { "type": "declaration", "name": "defaults" }
+    };
+
+    for (var key in config) {
+
+
+    }
+
+    return config;
+
+};
