ff62d07ab5e56a9bf91bc410b6f842757c9dba0c
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-02-12 23:51:34 -0800

    Optimize padding calculation by preventing getting text bounds when the users overrides padding left or bottom

--- src/scripts/core/cartesian.js
+++ src/scripts/core/cartesian.js
@@ -95,24 +95,28 @@
 
 
             adjustPadding: function () {
-                var yOptions = this.options.yAxis;
                 var xOptions = this.options.xAxis;
-                var yLabels = _.nw.extractScaleDomain(this.yDomain.slice().concat([_.nw.niceRound(this.yDomain[1])]), yOptions.min, yOptions.max);
-                var xLabels = this.xDomain;
-
-                var format = yOptions.labels.formatter || d3.format(yOptions.labels.format || ',.0f');
-                var yAxisText = _.map(yLabels, format).join('<br>');
-                var xAxisText = xLabels.join('<br>');
-                var yLabelBounds = _.nw.textBounds(yAxisText, '.y.axis');
-                var xLabelBounds = _.nw.textBounds(xAxisText, '.x.axis');
-                var regularXBounds = _.nw.textBounds('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890', '.x.axis');
-                var em = regularXBounds.height;
-                var ang = xOptions.labels && xOptions.labels.rotation ? xOptions.labels.rotation % 360 : 0;
-                var xLabelHeightUsed = ang === 0 ? regularXBounds.height : Math.ceil(Math.abs(xLabelBounds.width * Math.sin(_.nw.degToRad(ang))));
+                var yOptions = this.options.yAxis;
                 var maxTickSize = function (options) { return Math.max(options.outerTickSize || 0, options.innerTickSize || 0); };
+                if (!this.options.chart.padding.bottom) {
+                    var xLabels = this.xDomain;
+                    var xAxisText = xLabels.join('<br>');
+                    var xLabelBounds = _.nw.textBounds(xAxisText, '.x.axis');
+                    var regularXBounds = _.nw.textBounds('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890', '.x.axis');
+                    var em = regularXBounds.height;
+                    var ang = xOptions.labels && xOptions.labels.rotation ? xOptions.labels.rotation % 360 : 0;
+                    var xLabelHeightUsed = ang === 0 ? regularXBounds.height : Math.ceil(Math.abs(xLabelBounds.width * Math.sin(_.nw.degToRad(ang))));
+                    this.options.chart.padding.bottom = this.options.chart.padding.bottom || maxTickSize(this.options.xAxis) + (this.options.xAxis.tickPadding || 0) + xLabelHeightUsed + Math.ceil(em * Math.cos(_.nw.degToRad(ang)));
+                }
 
-                this.options.chart.padding.left = this.options.chart.padding.left ||  maxTickSize(this.options.yAxis) + (this.options.yAxis.tickPadding || 0) + yLabelBounds.width;
-                this.options.chart.padding.bottom = this.options.chart.padding.bottom || maxTickSize(this.options.xAxis) + (this.options.xAxis.tickPadding || 0) + xLabelHeightUsed + Math.ceil(em * Math.cos(_.nw.degToRad(ang)));
+                if (!this.options.chart.padding.left) {
+                    var yLabels = _.nw.extractScaleDomain(this.yDomain.slice().concat([_.nw.niceRound(this.yDomain[1])]), yOptions.min, yOptions.max);
+
+                    var format = yOptions.labels.formatter || d3.format(yOptions.labels.format || ',.0f');
+                    var yAxisText = _.map(yLabels, format).join('<br>');
+                    var yLabelBounds = _.nw.textBounds(yAxisText, '.y.axis');
+                    this.options.chart.padding.left = this.options.chart.padding.left ||  maxTickSize(this.options.yAxis) + (this.options.yAxis.tickPadding || 0) + yLabelBounds.width;
+                }
             },
 
             adjustTitlePadding: function () {
