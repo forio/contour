d27c6b2420c3417f11d31e159cd7c4e73547986c
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-13 16:56:22 -0700

    Created base unit test for legend

---
+++ tests/specs/legend-spec.js
@@ -0,0 +1,34 @@
+describe('Legend Visualization', function () {
+    var $el, el;
+    var instance;
+
+
+    beforeEach(function () {
+        $el = $('<div>');
+        el = $el.get(0);
+    });
+
+    function createinstance(options) {
+        $el = $('<div>');
+        el = $el.get(0);
+        options = _.extend({ el: el }, options);
+
+        instance = new Contour(options).cartesian();
+        return instance;
+    }
+
+    it('should create a div with the text for each data series', function () {
+        var data = [
+            { name: 'name-1', data: [] },
+            { name: 'name-2', data: [] }
+        ];
+
+        createinstance().nullVis(data).legend(data).render();
+
+        var entries = $el.find('.legend .legend-entry');
+        expect(entries.length).toBe(2);
+        expect(entries.eq(0).text()).toBe('name-1');
+        expect(entries.eq(1).text()).toBe('name-2');
+    });
+
+});
