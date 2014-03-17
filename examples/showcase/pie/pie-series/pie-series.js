b162439fd88da9f864b1d5e3ab91c9f931da85e8
Molly Jones <mjones@forio.com>
2014-03-17 15:28:51 -0700

    giant checkin -- renaming everything to "demo.*" for jsfiddle compatibility. minor updates, but not the fixed format.

--- examples/showcase/pie/pie-series/pie-series.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    new Contour({
-            el: '.pie-series',
-            pie: { piePadding: 15 }
-        })
-        .pie([{ data: [ 1, 2, 3, 4 ] }, { data: [ 2, 2, 5, 1 ] }, { data: [7, 4, 2, 2, 5] }])
-        .tooltip()
-        .render();
-})();
