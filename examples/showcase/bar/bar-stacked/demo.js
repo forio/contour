7b09318e5ae925af7da52a054261adc0fe9c0cf9
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-24 16:54:05 -0700

    Styled stacked bar gallery example

--- examples/showcase/bar/bar-stacked/demo.js
+++ examples/showcase/bar/bar-stacked/demo.js
@@ -1,34 +1,41 @@
 $(function () {
-    var data = [
-        {
-            name: 'First Series',
-            data: [1,2,3,4,2]
-        },
-        {
-            name: 'Second Series',
-            data: [1,1,1,1,8]
-        },
-        {
-            name: 'More Stuff',
-            data: [4,4,4,2,3]
-        }
-    ];
+
+    var data = [{
+        name: "Gold medals",
+        data: [{ x: "United States", y: 552 }, { x: "Russia", y: 234 }, { x: "China", y: 234 }, { x: "Germany", y: 223 }, { x: "Canada", y: 168 }, { x: "Australia", y: 163 }, { x: "Great Britain", y: 124 }, { x: "South Korea", y: 110 }, { x: "France", y: 108 }, { x: "Netherlands", y: 101 }]
+    }, {
+        name: "Silver medals",
+        data: [{ x: "United States", y: 440 }, { x: "Russia", y: 221 }, { x: "China", y: 156 }, { x: "Germany", y: 183 }, { x: "Canada", y: 98 }, { x: "Australia", y: 226 }, { x: "Great Britain", y: 101 }, { x: "South Korea", y: 93 }, { x: "France", y: 107 }, { x: "Netherlands", y: 135 }]
+    }, {
+        name: "Bronze medals",
+        data: [{ x: "United States", y: 320 }, { x: "Russia", y: 313 }, { x: "China", y: 140 }, { x: "Germany", y: 223 }, { x: "Canada", y: 104 }, { x: "Australia", y: 220 }, { x: "Great Britain", y: 97 }, { x: "South Korea", y: 105 }, { x: "France", y: 103 }, { x: "Netherlands", y: 82 }]
+    }];
 
     new Contour({
             el: '.bar-stacked',
-            xAxis: {
-                title: 'Category',
+
+            chart: {
+                padding: {
+                    left: 110
+                }
             },
+
             yAxis: {
-                title: 'Value'
+                title: 'Total Medals'
             },
+
             bar: {
                 stacked: true
+            },
+
+            legend: {
+                vAlign: 'top'
             }
         })
         .cartesian()
         .horizontal()
         .bar(data)
         .legend(data)
+        .tooltip()
         .render();
 });
