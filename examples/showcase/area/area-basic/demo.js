45f0c403a451d719673449f11aa24352c1b80f78
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-24 14:18:44 -0700

    Updated area-basic showcase example

--- examples/showcase/area/area-basic/demo.js
+++ examples/showcase/area/area-basic/demo.js
@@ -1,18 +1,42 @@
 $(function () {
+    var housePrice = [
+        { x: 2000, y: 418530 },
+        { x: 2001, y: 432050 },
+        { x: 2002, y: 410865 },
+        { x: 2003, y: 465025 },
+        { x: 2004, y: 548088 },
+        { x: 2005, y: 565843 },
+        { x: 2006, y: 598243 },
+        { x: 2007, y: 535018 },
+        { x: 2008, y: 498356 },
+        { x: 2009, y: 542686 },
+        { x: 2010, y: 652301 },
+        { x: 2011, y: 695342 },
+        { x: 2012, y: 752354 },
+        { x: 2013, y: 780543 }
+    ];
+
+    var moneyFormatter = d3.format('$,.0f');
     new Contour({
             el: '.area-basic',
+
             xAxis: {
-                title: 'Index',
+                title: 'Year'
             },
+
             yAxis: {
-                title: 'Value'
+                title: 'Value (US $)'
+            },
+
+            tooltip: {
+                formatter: function (d) {
+                    return 'Average house price<br>Year: ' + d.x + '<br>' + moneyFormatter(d.y);
+                }
             }
         })
         .cartesian()
-        .area([
-            { data: [10,20,5,14,54,23,12,45] },
-            { data: [40,32,32,43,65,73,12,33] },
-            ])
+        .area(housePrice)
+        .line(housePrice, { marker: { size: 3 } } )
         .tooltip()
         .render();
 });
