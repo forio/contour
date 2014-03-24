f757a986770faec94b3ee008494a2e33d65a6be4
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-24 15:52:23 -0700

    Clean up and styling for bar-export showcase example

--- examples/showcase/bar/bar-export/demo.js
+++ examples/showcase/bar/bar-export/demo.js
@@ -1,28 +1,42 @@
 $(function () {
-    Contour.export('customTooltipSingleSeries', function (data, layer) {
-    	// data[0] is the first data series.
-    	// because our data, below, only includes one data series, 
-    	// we can get the data series values 
-    	// by looping through the elements of data[0].data
-        for (var i=0; i < data[0].data.length; i++) {
-        	// use d3 to draw the text for the tooltip
-            layer.append('text')
-               .attr('class', 'axis')
-               .attr('y', this.xScale(data[0].data[i].x)+this.rangeBand/2)
-               .attr('x', this.yScale(data[0].data[i].y)+10)
-               .text(data[0].data[i].y)
-        }
+    Contour.export('customLegend', function (data, layer, options) {
+        var moneyFormatter = d3.format('.4s');
+        var barCenter = this.rangeBand / 2;
+        var x = this.xScale, y = this.yScale;
 
+        // use d3 style enter/update/exit to render the data labels
+        layer.selectAll('.label-text')
+            .data(data[0].data)
+            .enter().append('text')
+                .transition().delay(options.chart.animations.duration)
+                .attr('class', 'label-text')
+                .attr('y', function (d) { return x(d.x) + barCenter; })
+                .attr('x', function (d) { return y(d.y); })
+                .attr('dy', '.3em')
+                .attr('dx', '-.3em')
+                .attr('text-anchor', 'end')
+                .text(function (d) { return moneyFormatter(d.y); });
     });
 
-    var data = [1, 2, 3, 4, 5, 4, 3, 2, 1];
+    var estateTaxCollection = [23565164, 26717493, 24557815, 26543433, 21583131, 16930741];
 
     new Contour({
-        el: '.bar-export',
-      })
-    .cartesian()
-    .horizontal()
-    .bar(data)
-    .customTooltipSingleSeries(data)
-    .render();
+            el: '.bar-export',
+            xAxis: {
+                categories: ['2005', '2006', '2007', '2008', '2009', '2010']
+            },
+            yAxis: {
+                // this set of options will turn off all ticks on the yAxis
+                ticks: 0,
+                tickPadding: 0,
+                innerTickSize: 0,
+                outerTickSize: 0
+            }
+        })
+        .cartesian()
+        .horizontal()
+        .bar(estateTaxCollection)
+        // add our custom legend to our visualization instance
+        .customLegend(estateTaxCollection)
+        .render();
 });
