9cc17607a963c84f9bec6c031c643e114ee4a3b8
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-24 14:53:59 -0700

    Styles area-combo showcase chart

--- examples/showcase/area/area-combo/demo.js
+++ examples/showcase/area/area-combo/demo.js
@@ -1,20 +1,67 @@
 $(function () {
-    var data = [
-        { data: [10,20,5,14,54,23,12,45] }
+
+    var salesData = [
+        {
+            name: 'East Region',
+            data: [
+                { x: new Date('01-01-2010'), y: 1800 },
+                { x: new Date('02-01-2010'), y: 1530 },
+                { x: new Date('03-01-2010'), y: 1650 },
+                { x: new Date('04-01-2010'), y: 950 },
+                { x: new Date('05-01-2010'), y: 450 },
+                { x: new Date('06-01-2010'), y: 750 },
+                { x: new Date('07-01-2010'), y: 1480 },
+                { x: new Date('08-01-2010'), y: 2604 },
+                { x: new Date('09-01-2010'), y: 1350 },
+                { x: new Date('10-01-2010'), y: 1150 },
+                { x: new Date('11-01-2010'), y: 850 },
+                { x: new Date('12-01-2010'), y: 720 }
+            ]
+        }, {
+            name: 'West Region',
+            data: [
+                { x: new Date('01-01-2010'), y: 1917 },
+                { x: new Date('02-01-2010'), y: 1630 },
+                { x: new Date('03-01-2010'), y: 1656 },
+                { x: new Date('04-01-2010'), y: 1048 },
+                { x: new Date('05-01-2010'), y: 439 },
+                { x: new Date('06-01-2010'), y: 612 },
+                { x: new Date('07-01-2010'), y: 1368 },
+                { x: new Date('08-01-2010'), y: 2527 },
+                { x: new Date('09-01-2010'), y: 1350 },
+                { x: new Date('10-01-2010'), y: 1403 },
+                { x: new Date('11-01-2010'), y: 1161 },
+                { x: new Date('12-01-2010'), y: 596 }
+            ]
+        }
     ];
 
     new Contour({
             el: '.area-combo',
+            chart: {
+                gridlines: 'horizontal'
+            },
             xAxis: {
-                title: 'Index',
+                labels: {
+                    // x Axis labels should show abbreviated month only
+                    // different time formatting options can be found at
+                    // https://github.com/mbostock/d3/wiki/Time-Formatting
+                    format: '%b'
+                }
             },
             yAxis: {
-                title: 'Value'
+                title: 'Sales (x1,000)'
+            },
+            tooltip: {
+                formatter: function (d) {
+                    // format the tooltip as currency
+                    return d3.format('$,.0f')(d.y * 1000);
+                }
             }
         })
         .cartesian()
-        .area(data)
-        .line(data, { marker: { size: 3 } } )
+        .area(salesData)
+        .legend(salesData)
         .tooltip()
         .render();
 });
