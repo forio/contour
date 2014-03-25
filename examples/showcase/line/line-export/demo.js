d4b7dd4232ab445b2fb43e7584bef9b305d096f2
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-25 16:31:17 -0700

    Style and dataset update to line-export showcase example

--- examples/showcase/line/line-export/demo.js
+++ examples/showcase/line/line-export/demo.js
@@ -1,27 +1,51 @@
 $(function () {
 
     Contour.export('lineMarker', function (data, layer) {
-            var y = this.yScale(data);
-            // use d3 to draw the path for this visualization
-            layer.append('path')
-                .attr('d', 'M0,' + y + ' L' + this.options.chart.plotWidth + ',' + y)
-                .attr('stroke', '#000');     
+
+        var y = this.yScale(data.value);
+
+        // use d3 to draw the path for this visualization
+        // margin the range
+        layer.append('path')
+            .attr('d', 'M0,' + y + ' L' + this.options.chart.plotWidth + ',' + y)
+            .attr('class', data.cssClass);
+
+        layer.append('text')
+            .attr('class', 'line-marker-text')
+            .attr('x', this.options.chart.plotWidth)
+            .attr('y', y)
+            .attr('dy', '1em')
+            .attr('text-anchor', 'end')
+            .text(data.label);
+
+        layer.append('text')
+            .attr('class', 'line-marker-text')
+            .attr('x', this.options.chart.plotWidth)
+            .attr('y', y)
+            .attr('dy', '-.31em')
+            .attr('text-anchor', 'end')
+            .text(data.value);
+
     });
+
+    var data = [ 83, 101, 116, 109, 109, 75, 84, 98, 105, 76, 98, 113, 124, 101, 115, 122, 101, 100, 82, 110, 107, 85, 105, 75, 92, 82, 83, 101, 85, 120, 96, 104, 120, 120, 116, 99, 113, 105, 122 ];
 
     new Contour({
             el: '.line-export',
             xAxis: {
-                title: 'Index',
+                type: 'linear',
+                title: 'Sample No.',
             },
             yAxis: {
-                title: 'Value'
+                title: 'Speed (mm/s)',
+                min: 50
             }
         })
         .cartesian()
-        .line([{x: 0, y: 103},{x: 44, y: 103},{x: 154, y: 36},{x: 309, y: 150},{x: 376, y: 150},{x: 400, y: 171}])
-        .lineMarker(50)
-        .lineMarker(100)
-        .lineMarker(150)
+        .lineMarker({ value: 70, label: 'Low', cssClass: 'line-marker low-mark' })
+        .lineMarker({ value: 90, label: 'Expected', cssClass: 'line-marker expected-mark' })
+        .lineMarker({ value: 125, label: 'High', cssClass: 'line-marker high-mark' })
+        .line(data)
         .tooltip()
         .render();
 });
