f990bbd32fa9ac2208990f9657cf2385b314d233
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-25 13:12:37 -0700

    Styled and updated line-sine showcase example

--- examples/showcase/line/line-sine/demo.js
+++ examples/showcase/line/line-sine/demo.js
@@ -1,26 +1,52 @@
-
 $(function () {
     var pi = Math.PI;
 
+    var data = [{
+        name: 'Sine Curve',
+        data: [
+            {x: 0,          y: 0 },
+            {x: pi / 4,     y: 0.707 },
+            {x: pi / 2,     y: 1 },
+            {x: 3 * pi / 4, y: 0.707 },
+            {x: pi,         y: 0 },
+            {x: 5 * pi / 4, y: -0.707 },
+            {x: 3 * pi / 2, y: -1 },
+            {x: 7 * pi / 4, y: -0.707 },
+            {x: 2 * pi,     y: 0}
+        ]
+    }];
+
     new Contour({
             el: '.line-sine',
+            chart: {
+                gridlines: 'both',
+                animations: false
+            },
             xAxis: {
-                title: 'Index',
+                type: 'linear',
+                labels: {
+                    format: '.2f'
+                }
             },
             yAxis: {
-                title: 'Value'
+                labels: {
+                    format: '.2f'
+                }
             },
             line: {
                 smooth: true,
-                marker: { enable: false }
+                marker: {
+                    enable: false
+                }
             },
-            chart: {
-                gridlines: 'both'
+            legend: {
+                vAlign: 'top'
             }
         })
         .cartesian()
-        .line([{x: 0, y: 0},{x: pi / 4, y: .707},{x: pi / 2, y: 1},{x: 3 * pi / 4, y: .707},{x: pi, y: 0},{x: 5 * pi / 4, y: -.707},{x: 3 * pi / 2, y: -1},{x: 7 * pi / 4, y: -.707},{x: 2 * pi, y: 0}])
+        .line(data)
         .tooltip()
+        .legend(data)
         .render();
 });
 
