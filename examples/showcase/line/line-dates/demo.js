586b6d60693c736b058035725460f52e9eb41d6d
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-25 13:23:07 -0700

    styled and dataset update for line-dates showcase example

--- examples/showcase/line/line-dates/demo.js
+++ examples/showcase/line/line-dates/demo.js
@@ -1,21 +1,37 @@
 $(function () {
 
     var data = [
-        { x: new Date('1/1/2000'), y: 5},
-        { x: new Date('2/1/2000'), y: 3},
-        { x: new Date('3/1/2000'), y: 6},
-        { x: new Date('4/1/2000'), y: 7},
-        { x: new Date('5/1/2000'), y: 4},
-        { x: new Date('6/1/2000'), y: 2}
+        { x: new Date('3/19/2014'), y: 4307.6 },
+        { x: new Date('3/18/2014'), y: 4333.31 },
+        { x: new Date('3/17/2014'), y: 4279.95 },
+        { x: new Date('3/14/2014'), y: 4245.4 },
+        { x: new Date('3/13/2014'), y: 4260.42 },
+        { x: new Date('3/12/2014'), y: 4323.33 },
+        { x: new Date('3/11/2014'), y: 4307.19 },
+        { x: new Date('3/10/2014'), y: 4334.45 },
+        { x: new Date('3/7/2014'), y: 4336.22 },
+        { x: new Date('3/6/2014'), y: 4352.13 },
+        { x: new Date('3/5/2014'), y: 4357.97 },
+        { x: new Date('3/4/2014'), y: 4351.97 },
+        { x: new Date('3/3/2014'), y: 4277.3 }
     ];
 
     new Contour({
             el: '.line-dates',
+            chart: {
+                gridlines: 'horizontal',
+                padding: {
+                    right: 40
+                }
+            },
             xAxis: {
-                title: 'Index',
+                maxTicks: 7,
+                linearDomain: true,
             },
             yAxis: {
-                title: 'Value'
+                title: 'Value (US$)',
+                min: 4000,
+                max: 4500
             }
         })
         .cartesian()
