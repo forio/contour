16faa80d524ef4f43ac079dabd68592c4c5a7bf5
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-24 21:46:16 -0700

    styled column grouped showcase example

--- examples/showcase/column/column-grouped/demo.js
+++ examples/showcase/column/column-grouped/demo.js
@@ -1,23 +1,36 @@
 $(function () {
 
+    var moneyFormatter = d3.format('$,.0f');
     var groupedColData = [
-        { name: 'First Series', data: [6,5,3,5,7,6,3] },
-        { name: 'Second Series', data: [1,2,1,2,1,2,1] },
-        { name: 'Third Series', data: [6,5,1,5,2,3,5] },
-        { name: 'Fourth Series', data: [5,3,2,5,1,6,5] }
+        { name: '2012', data: [38535, 29533, 32423, 43023, 45055, 50323, 49231, 54023, 43025, 60122, 61342, 60234] },
+        { name: '2013', data: [34511, 34965, 36913, 39834, 50385, 57873, 38453, 28800, 46830, 33204, 56963, 46605] }
     ];
 
     new Contour({
             el: '.column-grouped',
             xAxis: {
-                title: 'Index or Category',
+                categories: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
             },
             yAxis: {
-                title: 'Value'
+                title: 'Sales'
+            },
+            legend: {
+                vAlign: 'bottom',
+                hAlign: 'center',
+                direction: 'horizontal'
+            },
+            tooltip: {
+                formatter: function (d) {
+                    return d.x + '<br>' + moneyFormatter(d.y);
+                }
             }
         })
         .cartesian()
         .column(groupedColData)
-        .stackTooltip(groupedColData, { el: '.column-stack-tooltip' })
+        .legend(groupedColData)
+        .tooltip()
         .render();
 });
+
+
+
