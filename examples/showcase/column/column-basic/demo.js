20ae17e78df32790aa247663e4607fc2075c1676
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-24 21:10:22 -0700

    Styled column basic showcase example

--- examples/showcase/column/column-basic/demo.js
+++ examples/showcase/column/column-basic/demo.js
@@ -1,15 +1,21 @@
 $(function () {
+
+    var data = [7.6, 14.0, 7.5, 24.3, 11.1, 35.3, 8.4];
+
     new Contour({
             el: '.column-basic',
             xAxis: {
-                title: 'Index or Category',
+                categories: ['Milk (Whole)', 'Milk (Reduced Fat)', 'Tea', 'Coffee', 'Soft Drink (Diet)', 'Soft Drink (Reg)', 'Fruit juices'],
+                labels: {
+                    rotation: -45
+                }
             },
             yAxis: {
-                title: 'Value'
+                title: 'Gal. per Capita'
             }
         })
         .cartesian()
-        .column([1, 2, 3, 4, 5, 4, 3, 2, 1])
+        .column(data)
         .tooltip()
         .render();
 });
