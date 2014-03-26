9b036cc0b72623985266472871829d88b00df1f6
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-25 21:56:21 -0700

    Styled pie series showcase example

--- examples/showcase/pie/pie-series/demo.js
+++ examples/showcase/pie/pie-series/demo.js
@@ -1,9 +1,42 @@
 $(function () {
+
+    Contour.export('pieName', function (data, layer, options) {
+
+        var w = options.chart.plotWidth;
+        var padding = options.pie.piePadding - 10;
+        var paddings = (w / data.length / 2) - padding;
+        var pieRadius = (w - paddings) / data.length;
+        layer.selectAll('.pie-name')
+            .data(data)
+            .enter().append('text')
+                .attr('class', 'pie-name')
+                .attr('x', function (d, i) {
+                    return options.pie.piePadding * (i + 1) + (pieRadius ) * i + pieRadius / 2;
+                })
+                .attr('text-anchor', 'middle')
+                .attr('y', (options.pie.piePadding + pieRadius + 25))
+                .text(function (d) { return d.name; });
+    });
+
+    var data = [{
+        name: 'Germany',
+        data: [{ x: 'Prod A', y: 1 }, { x: 'Prod B', y: 2 }, { x: 'Prod C', y: 3 }, { x: 'Prod D', y: 4 }]
+    }, {
+        name: 'France',
+        data: [{ x: 'Prod A', y: 2 }, { x: 'Prod B', y: 2 }, { x: 'Prod C', y: 5 }, { x: 'Prod D', y: 1 }]
+    }, {
+        name: 'Spain',
+        data: [{ x: 'Prod A', y: 7 }, { x: 'Prod B', y: 4 }, { x: 'Prod C', y: 2 }, { x: 'Prod D', y: 2 }]
+    }];
+
     new Contour({
             el: '.pie-series',
-            pie: { piePadding: 15 }
+            pie: {
+                piePadding: 15,
+            }
         })
-        .pie([{ data: [ 1, 2, 3, 4 ] }, { data: [ 2, 2, 5, 1 ] }, { data: [7, 4, 2, 2, 5] }])
+        .pie(data)
+        .pieName(data)
         .tooltip()
         .render();
 });
