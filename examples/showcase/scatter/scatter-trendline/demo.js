4aab55905f723d63a0be0cf17cef8b398eb282a6
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-25 22:25:43 -0700

    Styled scatter trend line showcase example

--- examples/showcase/scatter/scatter-trendline/demo.js
+++ examples/showcase/scatter/scatter-trendline/demo.js
@@ -1,38 +1,58 @@
 $(function () {
-    series = [];
-    // series defined as 3x10 array of random (x,y) pairs
+    var series = [];
+    var names = ['School A', 'School B', 'School C'];
+    var rand = function(l,h) { return Math.random() * (h-l) + l; };
 
+    // series defined as 3x10 array of random (x,y) pairs
     for(var j=0; j<3; j++) {
         var data = [];
         series.push({
+            name: names[j],
             data: data
         });
 
-        var rand = function(l,h) { return Math.random() * (h-l) + l; };
-
-        for(var k=0; k<10; k++) {
+        for(var k=0; k<20; k++) {
             data.push({
-                x: k + rand(-1, 1),
-                y: k + rand(0, 5)
+                x: k + rand(0, 1),
+                y: k + rand(0, 15)
             });
         }
     }
 
+    Contour.export('r2Info', function (data, layer, options) {
+        var reg = _.nw.linearRegression(_.flatten(_.pluck(data, 'data')));
+
+        layer.append('text')
+            .attr('class', 'r2-info')
+            .attr('x', options.chart.plotWidth)
+            .attr('y', options.chart.plotHeight)
+            // .attr('dx', '-1em')
+            .attr('dy', '-1em')
+            .attr('text-anchor', 'end')
+            .text('R2: ' + d3.format('.3f')(reg.r2));
+    });
+
     new Contour({
             el: '.scatter-trendline',
             xAxis: {
-                title: 'Value'
+                title: 'Study Hours'
             },
             yAxis: {
-                title: 'Value'
+                title: 'Score'
             },
             scatter: {
                 radius: 2.5
+            },
+            legend: {
+                vAlign: 'top',
+                hAlign: 'left'
             }
         })
         .cartesian()
         .scatter(series)
         .trendLine(series)
+        .legend(series)
+        .r2Info(series)
         .tooltip()
         .render();
 });
