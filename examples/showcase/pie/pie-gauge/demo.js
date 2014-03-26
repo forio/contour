427b733376ec3f61d28ed3cde37ac84d950d03ee
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-25 21:24:49 -0700

    Styled pie-gauge showcase example

--- examples/showcase/pie/pie-gauge/demo.js
+++ examples/showcase/pie/pie-gauge/demo.js
@@ -5,23 +5,24 @@ $(function () {
     Contour.export('donutTextOneValue', function (data, layer, options) {
 
         // This visualization is only for single-element gauges, that is,
-        // donut (pie) charts with one data series, one value visible, and the remainder hidden.
+        // donut (pie) charts with one data series, one value visible, and the remainder dimed out.
         // So we can assume that there are only two elements in the data series.
-
-        var greater;
-
-        if (data[0].data[0].y > data[0].data[1].y)
-            { greater = 0; }
-        else { greater = 1; }
-
+        var visibleIndex = data[0].data[0].y < data[0].data[1].y ? 1 : 0;
+        var center = options.pie.piePadding + options.pie.outerRadius;
 
         layer.append('text')
-            .attr('class', 'axis')
-            .attr('y', options.pie.piePadding + options.pie.outerRadius)
-            .attr('x', options.pie.piePadding + options.pie.outerRadius)
-            .attr('style', 'text-anchor: middle; font-size: 30')
+            .attr('class', 'center-text')
+            .attr('x', center)
+            .attr('y', center)
             .attr('dy', '.3em')
-            .text(formatter(data[0].data[greater].y));
+            .text(formatter(data[0].data[visibleIndex].y));
+
+        layer.append('text')
+            .attr('class', 'center-label')
+            .attr('x', center)
+            .attr('y', center)
+            .attr('dy', '2.3em')
+            .text('Growth Rate');
     });
 
     var data = [{ x: 'Case A', y: 0.82}, { x: 'Case B', y: 0.18 }];
@@ -37,7 +38,6 @@ $(function () {
         })
         .pie(data)
         .donutTextOneValue(data)
-        .tooltip()
         .render();
 });
 
