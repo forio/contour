b92db69ca3a69895bd048fa686891eefa52d8b4b
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-28 14:12:31 -0700

    Moved bar-basic to bar-styled and made bar-basic really basic

--- examples/showcase/bar/bar-basic/demo.js
+++ examples/showcase/bar/bar-basic/demo.js
@@ -1,28 +1,13 @@
 $(function () {
 
-    var browserPenetration =[1.14, 8.51, 18.37, 28.54, 40.88];
+    // sample data
+    var data =[21, 15, 23, 24, 29, 4, 13, 8, 14, 7, 6, 3];
 
     new Contour({
             el: '.bar-basic',
-            xAxis: {
-                categories: ['Opera', 'Safari', 'Firefox', 'IE', 'Chrome']
-            },
-            yAxis: {
-                labels: {
-                    formatter: function (d) {
-                        return d + '%';
-                    }
-                }
-            },
-            tooltip: {
-                formatter: function (d) {
-                    return '<strong>' + d.x + '</strong><br>' + d.y + '%';
-                }
-            }
         })
         .cartesian()
         .horizontal()
-        .bar(browserPenetration)
-        .tooltip()
+        .bar(data)
         .render();
 });
