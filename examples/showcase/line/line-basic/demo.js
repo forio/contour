f45be06cc5215fcaedfbaa8c4685b215476516e2
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-28 13:54:11 -0700

    Added comments to gallery example

--- examples/showcase/line/line-basic/demo.js
+++ examples/showcase/line/line-basic/demo.js
@@ -1,11 +1,18 @@
 $(function () {
 
+    // sample data set
     var data = [22, 8, 5, 19, 11, 4, 5, 13, 20, 29, 25];
 
+    // we create a Contour instance
     new Contour({
-        el: '.chart'
+        el: '.chart',
     })
+    // adding a cartesian frame to the instance gives use an X and Y axis
+    // and provide scaling service for the visualizations
     .cartesian()
+    // we now add a line visualization to the instance
+    // and we pass in the data we want to use
     .line(data)
+    // finally we call render to draw the chart and visualizations onto the web page (DOM)
     .render();
 });
