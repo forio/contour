be5816b080a69175bebf08d143928e48a1f4010d
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-03-28 14:39:02 -0700

    Added donut series showcase example

---
+++ examples/showcase/pie/donut-series/demo.js
@@ -0,0 +1,52 @@
+$(function () {
+
+    Contour.export('pieName', function (data, layer, options) {
+
+        var w = options.chart.plotWidth;
+        var padding = options.pie.piePadding - 10;
+        var paddings = (w / data.length / 2) - padding;
+        var pieRadius = (w - paddings) / data.length;
+        layer.selectAll('.pie-name')
+            .data(data)
+            .enter().append('text')
+                .attr('class', 'pie-name')
+                .attr('x', function (d, i) {
+                    return options.pie.piePadding * (i + 1) + (pieRadius ) * i + pieRadius / 2;
+                })
+                .attr('text-anchor', 'middle')
+                .attr('y', (options.pie.piePadding + pieRadius / 2 + 5))
+                .text(function (d) { return d.name; });
+    });
+
+    var data = [{
+        name: 'Germany',
+        data: [{ x: 'Prod A', y: 1 }, { x: 'Prod B', y: 2 }, { x: 'Prod C', y: 3 }, { x: 'Prod D', y: 4 }]
+    }, {
+        name: 'France',
+        data: [{ x: 'Prod A', y: 2 }, { x: 'Prod B', y: 2 }, { x: 'Prod C', y: 5 }, { x: 'Prod D', y: 1 }]
+    }, {
+        name: 'Spain',
+        data: [{ x: 'Prod A', y: 7 }, { x: 'Prod B', y: 4 }, { x: 'Prod C', y: 2 }, { x: 'Prod D', y: 2 }]
+    }];
+
+    new Contour({
+            el: '.pie-series',
+            chart: {
+                height: 230
+            },
+            pie: {
+                piePadding: 15,
+                innerRadius: 50
+            },
+            legend: {
+                hAlign: 'center',
+                vAlign: 'bottom',
+                direction: 'horizontal'
+            }
+        })
+        .pie(data)
+        .pieName(data)
+        .legend(_.map(_.pluck(data[0].data, 'x'), function (x) { return { name: x, data: [] }; }))
+        .tooltip()
+        .render();
+});
