eeb19ee4c44bc4a09b8a2370fbe3e8bfca6c3a92
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-06-16 21:24:05 -0700

    Release 0.9.89

--- dist/contour.js
+++ dist/contour.js
@@ -1328,7 +1328,7 @@
 
 })();
 
-Contour.version = '0.9.88';
+Contour.version = '0.9.89';
 (function () {
 
     var helpers = {
@@ -3387,15 +3387,24 @@ Contour.export('nullVis', _.noop);
 
 (function () {
 
+    function normalizeDataSet(dataSet) {
+        var all = _.flatten(_.pluck(dataSet, 'data'));
+        var isLinear = all.length && _.isNumber(all[0].x);
+        var normalizer = function (d, i) { return { x: i, y: d.y }; }
 
-    function ctor(data, layer, options) {
+        return isLinear ? all : _.map(all, normalizer);
+    }
+
+    function ctor(raw, layer, options) {
         this.checkDependencies('cartesian');
+        var data = normalizeDataSet(raw);
         var duration = options.chart.animations.duration != null ? options.chart.animations.duration : 400;
         var shouldAnimate = options.chart.animations && options.chart.animations.enable;
         var x = _.bind(function(d) { return this.xScale(d) + this.rangeBand / 2; }, this);
         var y = _.bind(function(d) { return this.yScale(d); }, this);
-        var regression = _.nw.linearRegression(_.flatten(_.pluck(data, 'data')));
+        var regression = _.nw.linearRegression(data);
         var domain = d3.extent(this.xScale.domain());
+        var numericDomain = d3.extent(data, function(p) { return p.x; });
         var lineY = function (x) { return regression.intercept + regression.slope * x; };
 
         var line = layer.selectAll('.trend-line')
@@ -3404,25 +3413,20 @@ Contour.export('nullVis', _.noop);
         line.enter().append('line')
             .attr('class', 'trend-line')
             .attr('x1', x(domain[0]))
-            .attr('y1', y(lineY(domain[0])))
+            .attr('y1', y(lineY(numericDomain[0])))
             .attr('x2', x(domain[0]))
-            .attr('y2', y(lineY(domain[0])));
+            .attr('y2', y(lineY(numericDomain[0])));
 
         line.exit().remove();
 
         if (shouldAnimate) {
-            line.transition().duration(duration)
-                .attr('x1', x(domain[0]))
-                .attr('y1', y(lineY(domain[0])))
-                .attr('x2', x(domain[1]))
-                .attr('y2', y(lineY(domain[1])));
-        } else {
-            line
-                .attr('x1', x(domain[0]))
-                .attr('y1', y(lineY(domain[0])))
-                .attr('x2', x(domain[1]))
-                .attr('y2', y(lineY(domain[1])));
+            line = line.transition().duration(duration);
         }
+
+        line.attr('x1', x(domain[0]))
+            .attr('y1', y(lineY(numericDomain[0])))
+            .attr('x2', x(domain[1]))
+            .attr('y2', y(lineY(numericDomain[1])));
     }
 
     ctor.defaults = {};
--- src/scripts/core/version.js
+++ src/scripts/core/version.js
@@ -1,1 +1,1 @@
-Contour.version = '0.9.88';
+Contour.version = '0.9.89';
