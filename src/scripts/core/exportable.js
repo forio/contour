6255b752bbb7bb18084639bc01f21cbe58787bcb
David Rosen <drosen0@github.com>
2014-08-14 12:28:22 -0700

    fix automatic download of exported image on Firefox

--- src/scripts/core/exportable.js
+++ src/scripts/core/exportable.js
@@ -276,7 +276,9 @@
                             var a = document.createElement('a');
                             a.download = options.fileName;
                             a.href = canvasData;
+                            document.body.appendChild(a);
                             a.click();
+                            document.body.removeChild(a);
                         },
                         'place': function () {
                             var img = document.createElement('img');
