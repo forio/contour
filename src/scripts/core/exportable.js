db72c8004aab13db0f1d305713c3ed879688d4d4
David Rosen <drosen0@github.com>
2014-08-14 23:06:45 -0700

    move base64 encoder/decoder into shim.js

--- src/scripts/core/exportable.js
+++ src/scripts/core/exportable.js
@@ -10,8 +10,6 @@
         checked: false
     };
     var shim = { // shims depending on current browser
-        btoa: undefined, // base 64 encoder
-        atob: undefined, // base 64 decoder
         serializeXml: undefined // xml serializer
     };
 
@@ -107,14 +105,14 @@
 
             function encodeBase64DataUrl(svgXml) {
                 // https://developer.mozilla.org/en/DOM/window.btoa
-                return 'data:image/svg+xml;base64,' + shim.btoa(svgXml);
+                return 'data:image/svg+xml;base64,' + btoa(svgXml);
             }
 
             // convert base64/URLEncoded data component to raw binary data held in a string
             function dataUrlToBlob(dataUrl) {
                 var byteString;
                 if (dataUrl.split(',')[0].indexOf('base64') >= 0) {
-                    byteString = shim.atob(dataUrl.split(',')[1]);
+                    byteString = atob(dataUrl.split(',')[1]);
                 } else {
                     byteString = unescape(dataUrl.split(',')[1]);
                 }
@@ -360,143 +358,11 @@
         function checkEncodesBase64() {
             browser.encodesBase64 = !!window.btoa;
 
-            if (browser.encodesBase64) {
-                shim.btoa = function (s) { // use window.btoa for base64 encoding
-                    return btoa(s); // note that it won't work as just `shim.btoa = window.btoa;`, therefore it needs to be wrapped
-                };
-                shim.atob = function (s) { // use window.atob for base64 decoding
-                    return atob(s); // note that it won't work as just `shim.atob = window.atob;`, therefore it needs to be wrapped
-                };
-            } else {
-                shim.btoa = base64Encode; // use custom base64 encoder
-                shim.atob = base64Decode; // use custom base64 decoder
-            }
-
-
-            var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-
-            // base64 encode
-            // adapted from http://www.webtoolkit.info/javascript-base64.html
-            function base64Encode(input) {
-                var output = '';
-                var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-                var i = 0;
-
-                input = utf8Encode(input);
-
-                while (i < input.length) {
-                    chr1 = input.charCodeAt(i++);
-                    chr2 = input.charCodeAt(i++);
-                    chr3 = input.charCodeAt(i++);
-
-                    enc1 = chr1 >> 2;
-                    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-                    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-                    enc4 = chr3 & 63;
-
-                    if (isNaN(chr2)) {
-                        enc3 = enc4 = 64;
-                    } else if (isNaN(chr3)) {
-                        enc4 = 64;
-                    }
-
-                    output = output +
-                        keyStr.charAt(enc1) +
-                        keyStr.charAt(enc2) +
-                        keyStr.charAt(enc3) +
-                        keyStr.charAt(enc4);
-                }
-
-                return output;
-
-            }
-
-            function utf8Encode(string) {
-                string = string.replace(/\r\n/g, '\n');
-                var utftext = '';
-
-                for (var n = 0; n < string.length; n++) {
-                    var c = string.charCodeAt(n);
-
-                    if (c < 128) {
-                        utftext += String.fromCharCode(c);
-                    }
-                    else if((c > 127) && (c < 2048)) {
-                        utftext += String.fromCharCode((c >> 6) | 192);
-                        utftext += String.fromCharCode((c & 63) | 128);
-                    }
-                    else {
-                        utftext += String.fromCharCode((c >> 12) | 224);
-                        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-                        utftext += String.fromCharCode((c & 63) | 128);
-                    }
-                }
-
-                return utftext;
-            }
-
-            // base64 decode
-            // adapted from http://www.webtoolkit.info/javascript-base64.html
-            function base64Decode(input) {
-                var output = '';
-                var chr1, chr2, chr3;
-                var enc1, enc2, enc3, enc4;
-                var i = 0;
-
-                input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '');
-
-                while (i < input.length) {
-                    enc1 = keyStr.indexOf(input.charAt(i++));
-                    enc2 = keyStr.indexOf(input.charAt(i++));
-                    enc3 = keyStr.indexOf(input.charAt(i++));
-                    enc4 = keyStr.indexOf(input.charAt(i++));
-
-                    chr1 = (enc1 << 2) | (enc2 >> 4);
-                    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-                    chr3 = ((enc3 & 3) << 6) | enc4;
-
-                    output = output + String.fromCharCode(chr1);
-
-                    if (enc3 != 64) {
-                        output = output + String.fromCharCode(chr2);
-                    }
-                    if (enc4 != 64) {
-                        output = output + String.fromCharCode(chr3);
-                    }
-                }
-
-                output = utf8Decode(output);
-
-                return output;
-            }
-
-            function utf8Decode(utftext) {
-                var string = '';
-                var i = 0;
-                var c, c1, c2;
-                c = c1 = c2 = 0;
-
-                while (i < utftext.length) {
-                    c = utftext.charCodeAt(i);
-
-                    if (c < 128) {
-                        string += String.fromCharCode(c);
-                        i++;
-                    }
-                    else if ((c > 191) && (c < 224)) {
-                        c2 = utftext.charCodeAt(i + 1);
-                        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-                        i += 2;
-                    }
-                    else {
-                        c2 = utftext.charCodeAt(i + 1);
-                        c3 = utftext.charCodeAt(i + 2);
-                        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-                        i += 3;
-                    }
-                }
-
-                return string;
+            if (!browser.encodesBase64) { // load base64 shim for IE9
+                var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.src = 'shim.js';
+                document.head.appendChild(script);
             }
         }
 
