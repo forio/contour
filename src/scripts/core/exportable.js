1254746e5e1ea83a6862fa2a566a93ab7f85d074
David Rosen <drosen0@github.com>
2014-08-15 13:01:07 -0700

    move shims back into exportable.js

--- src/scripts/core/exportable.js
+++ src/scripts/core/exportable.js
@@ -7,7 +7,7 @@
     };
 
     var browser = { // browser capabilities
-        checked: false
+        checked: false // true after browser capabilities have been checked
     };
     var shim = { // shims depending on current browser
         serializeXml: undefined // xml serializer
@@ -340,61 +340,49 @@
     function checkBrowser() {
         browser.checked = true;
 
+        checkEncodesBase64();
+        checkHasTypedArray();
+        checkSerializesXml();
         checkADownloads();
         checkSavesMsBlobs();
-        checkEncodesBase64();
-        checkSerializesXml();
         checkExportsSvg();
 
 
-        function checkADownloads() {
-            browser.aDownloads = document.createElement('a').download !== undefined;
-        }
-
-        function checkSavesMsBlobs() {
-            browser.savesMsBlobs = !!navigator.msSaveOrOpenBlob;
-        }
-
         function checkEncodesBase64() {
-            browser.encodesBase64 = !!window.btoa;
+            browser.encodesBase64 = 'btoa' in window;
 
-            if (!browser.encodesBase64) { // load base64 shim for IE9
-                var script = document.createElement('script');
-                script.type = 'text/javascript';
-                script.src = 'shim.js';
-                document.head.appendChild(script);
+            // setup shim for IE9
+            if (!browser.encodesBase64) {
+                setupBase64Shim();
+            }
+        }
+
+        function checkHasTypedArray() {
+            browser.hasTypedArray = 'Uint8Array' in window;
+
+            if (!browser.hasTypedArray) {
+                setupTypedArrayShim();
             }
         }
 
         function checkSerializesXml() {
-            browser.serializesXml = !!window.XMLSerializer;
+            browser.serializesXml = 'XMLSerializer' in window;
 
             if (browser.serializesXml) {
                 shim.serializeXml = function (xml) { // use standard XMLSerializer.serializeToString
                     return (new XMLSerializer()).serializeToString(xml);
                 };
             } else {
-                shim.serializeXml = serializeXmlToString; // use custom serializeXmlToString for IE9
+                setupXmlSerializerShim();
             }
+        }
 
-            // quick-n-serialize an SVG dom, needed for IE9 where there's no XMLSerializer nor SVG.xml
-            // s: SVG dom, which is the <svg> elemennt
-            function serializeXmlToString(s) {
-                var out = '<' + s.nodeName;
-                for (var i = 0; i < s.attributes.length; i++) {
-                    out += ' ' + s.attributes[i].name + '=' + '"' + s.attributes[i].value + '"';
-                }
+        function checkADownloads() {
+            browser.aDownloads = document.createElement('a').download !== undefined;
+        }
 
-                if (s.hasChildNodes()) {
-                    out += '>\n';
-                    for (var j = 0; j < s.childNodes.length; j++) {
-                        out += serializeXmlToString(s.childNodes[j]);
-                    }
-                    out += '</' + s.nodeName + '>' + '\n';
-                } else out += ' />\n';
-
-                return out;
-            }
+        function checkSavesMsBlobs() {
+            browser.savesMsBlobs = 'msSaveOrOpenBlob' in navigator;
         }
 
         function checkExportsSvg() {
@@ -457,6 +445,195 @@
                         document.head.appendChild(script);
                     });
                 }
+            }
+        }
+
+        function setupBase64Shim() {
+            var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+            window.btoa = base64Encode;
+            window.atob = base64Decode;
+
+
+            // base64 encode
+            function base64Encode(input) {
+                var output = '';
+                var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+                var i = 0;
+
+                input = utf8Encode(input);
+
+                while (i < input.length) {
+                    chr1 = input.charCodeAt(i++);
+                    chr2 = input.charCodeAt(i++);
+                    chr3 = input.charCodeAt(i++);
+
+                    enc1 = chr1 >> 2;
+                    enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                    enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                    enc4 = chr3 & 63;
+
+                    if (isNaN(chr2)) {
+                        enc3 = enc4 = 64;
+                    } else if (isNaN(chr3)) {
+                        enc4 = 64;
+                    }
+
+                    output = output +
+                        keyStr.charAt(enc1) +
+                        keyStr.charAt(enc2) +
+                        keyStr.charAt(enc3) +
+                        keyStr.charAt(enc4);
+                }
+
+                return output;
+            }
+
+            function utf8Encode(string) {
+                string = string.replace(/\r\n/g, '\n');
+                var utftext = '';
+
+                for (var n = 0; n < string.length; n++) {
+                    var c = string.charCodeAt(n);
+
+                    if (c < 128) {
+                        utftext += String.fromCharCode(c);
+                    } else if((c > 127) && (c < 2048)) {
+                        utftext += String.fromCharCode((c >> 6) | 192);
+                        utftext += String.fromCharCode((c & 63) | 128);
+                    } else {
+                        utftext += String.fromCharCode((c >> 12) | 224);
+                        utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                        utftext += String.fromCharCode((c & 63) | 128);
+                    }
+                }
+
+                return utftext;
+            }
+
+            // base64 decode
+            function base64Decode(input) {
+                var output = '';
+                var chr1, chr2, chr3;
+                var enc1, enc2, enc3, enc4;
+                var i = 0;
+
+                input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '');
+
+                while (i < input.length) {
+                    enc1 = keyStr.indexOf(input.charAt(i++));
+                    enc2 = keyStr.indexOf(input.charAt(i++));
+                    enc3 = keyStr.indexOf(input.charAt(i++));
+                    enc4 = keyStr.indexOf(input.charAt(i++));
+
+                    chr1 = (enc1 << 2) | (enc2 >> 4);
+                    chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                    chr3 = ((enc3 & 3) << 6) | enc4;
+
+                    output = output + String.fromCharCode(chr1);
+
+                    if (enc3 != 64) {
+                        output = output + String.fromCharCode(chr2);
+                    }
+                    if (enc4 != 64) {
+                        output = output + String.fromCharCode(chr3);
+                    }
+                }
+
+                output = utf8Decode(output);
+
+                return output;
+            }
+
+            function utf8Decode(utftext) {
+                var string = '';
+                var i = 0;
+                var c, c1, c2;
+                c = c1 = c2 = 0;
+
+                while (i < utftext.length) {
+                    c = utftext.charCodeAt(i);
+
+                    if (c < 128) {
+                        string += String.fromCharCode(c);
+                        i++;
+                    } else if ((c > 191) && (c < 224)) {
+                        c2 = utftext.charCodeAt(i + 1);
+                        string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+                        i += 2;
+                    } else {
+                        c2 = utftext.charCodeAt(i + 1);
+                        c3 = utftext.charCodeAt(i + 2);
+                        string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+                        i += 3;
+                    }
+                }
+
+                return string;
+            }
+        }
+
+        function setupTypedArrayShim() {
+            window.Uint8Array = TypedArray;
+            window.Uint32Array = TypedArray;
+            window.Int32Array = TypedArray;
+
+
+            function subarray(start, end) {
+                return this.slice(start, end);
+            }
+         
+            function set_(array, offset) {
+                if (arguments.length < 2) offset = 0;
+                for (var i = 0, n = array.length; i < n; ++i, ++offset) {
+                    this[offset] = array[i] & 0xFF;
+                }
+            }
+         
+            // we need typed arrays
+            function TypedArray(arg1) {
+                var result;
+                if (typeof arg1 === 'number') {
+                    result = new Array(arg1);
+                    for (var i = 0; i < arg1; ++i) {
+                        result[i] = 0;
+                    }
+                } else {
+                    result = arg1.slice(0);
+                }
+                result.subarray = subarray;
+                result.buffer = result;
+                result.byteLength = result.length;
+                result.set = set_;
+                if (typeof arg1 === 'object' && arg1.buffer) {
+                    result.buffer = arg1.buffer;
+                }
+         
+                return result;
+            }
+        }
+
+        function setupXmlSerializerShim() {
+            shim.serializeXml = serializeXmlToString; // use custom serializeXmlToString for IE9
+
+
+            // quick-n-serialize an SVG dom, needed for IE9 where there's no XMLSerializer nor SVG.xml
+            // s: SVG dom, which is the <svg> elemennt
+            function serializeXmlToString(s) {
+                var out = '<' + s.nodeName;
+                for (var i = 0; i < s.attributes.length; i++) {
+                    out += ' ' + s.attributes[i].name + '=' + '"' + s.attributes[i].value + '"';
+                }
+
+                if (s.hasChildNodes()) {
+                    out += '>\n';
+                    for (var j = 0; j < s.childNodes.length; j++) {
+                        out += serializeXmlToString(s.childNodes[j]);
+                    }
+                    out += '</' + s.nodeName + '>' + '\n';
+                } else out += ' />\n';
+
+                return out;
             }
         }
     }
