e06b189545156d6e226fdeff897938c2a5bc0eac
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-12-26 12:24:12 -0800

    Added commonJS export code to contour.js file

--- src/scripts/core/contour.js
+++ src/scripts/core/contour.js
@@ -456,8 +456,14 @@
         }
     });
 
-
-    // export to our context
-    root.Contour = Contour;
+    // exports for commonJS and requireJS styles
+    if (typeof module === "object" && module && typeof module.exports === "object") {
+        module.exports = Contour;
+    } else {
+        root.Contour = Contour;
+        if (typeof define === "function" && define.amd) {
+            define("contour", [], function () { return Contour; });
+        }
+    }
 
 })();
--- src/scripts/header.js
+++ src/scripts/header.js
@@ -2,7 +2,7 @@
 
     var root = this;
 
-    if ( typeof module === "object" && module && typeof module.exports === "object" ) {
+    if (typeof module === "object" && module && typeof module.exports === "object") {
         if(typeof require === 'function') {
             root.d3 = require('d3');
             root._ = require('lodash');
@@ -12,4 +12,4 @@
     if(!d3) throw new Error('You need to include d3.js before Contour. Go to http://d3js.org/');
     if(!_ || !_.merge) throw new Error('You need to include lodash.js before Contour. Go to http://lodash.com/');
 
-
+})();
