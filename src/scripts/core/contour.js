665fb96bbfe8268c9d0dfe317513077e6cbbaff1
Dan Baker <dbaker@forio.com>
2015-03-10 17:02:09 -0400

    lastData should be kept on the instance and not in Contour itself.

--- src/scripts/core/contour.js
+++ src/scripts/core/contour.js
@@ -63,7 +63,7 @@
     // the need to specify it again.... this allows you to do
     // new Contour().cartesian().line(dataset).lengend().tooltip().render()
     // and legend and tooltip will recieve dataset
-    var lastData;
+    this.lastData;
 
     /**
     * Creates a Contour instance, based on the core Contour visualizations object. This instance can contain a set of related visualizations.
@@ -136,11 +136,11 @@
             var opt =  _.extend({}, this.options[ctorName], options);
             var vis;
 
-            data = data || lastData || [];
+            data = data || this.lastData || [];
             sortSeries(data);
             vis = new Contour.VisualizationContainer(data, categories, opt, ctorName, renderer, this);
             this._visualizations.push(vis);
-            lastData = data;
+            this.lastData = data;
             return this;
         };
 
